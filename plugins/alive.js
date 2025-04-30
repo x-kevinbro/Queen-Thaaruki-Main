88ce832bfeaf414fab9fbd378635186a0eb0161d
x-kevinbro <damiruxox@outlook.com>
2025-04-30 12:50:47 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -7,7 +7,7 @@ cmd({
     alias: ["status", "runtime", "uptime"],
     desc: "Check uptime and system status",
     category: "main",
-    react: "📟",
+    react: "🩵",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
