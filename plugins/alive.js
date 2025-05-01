aad1686e14e16369a58620122f2f1d7c3038bd5e
x-kevinbro <damiruxox@outlook.com>
2025-05-01 21:29:45 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,28 +13,31 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *𝐐𝐔𝚵𝚵𝐍 𝐑𝚫𝐒𝐇𝐔 𝐌𝐃* 〕━━┈⊷
+        const status = `╭━━━━━━〔 *𝚭𝚯𝚪𝚯 𝚭𝚳𝐃* 〕━━━━━━┈⊷
+    •••Ｈｅｌｌｏ ${pushname}👊,•••
+       🎐 Ｉ ａｍ Ａｌｉｖｅ Ｎｏｗ！ 🎐
+
 ┃◈╭─────────────·๏
 ┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 ┃◈┃• *⚙️ HostName*: ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: QUEEN-RASHU-MD
+┃◈┃• *👨‍💻 Owner*: ZORO-ZMD
 ┃◈┃• *🧬 Version*: 2.0.0
 ┃◈└───────────┈⊷
-╰──────────────┈⊷
-> *© 𝙿𝙾𝚆𝙴𝙰𝚁𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝚁𝙰𝚂𝙷𝚄 𝙼𝙳 ✾*`;
+╰────────────────────────┈⊷
+> *© 𝚉𝙾𝚁𝙾 𝚉𝙼𝙳 𝙼𝚄𝙻𝚃𝙸𝙳𝙴𝚅𝙸𝙲𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿-𝙱𝙾𝚃 ✾*`;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/BsjkCDP/9555.jpg` },  // Image URL
+            image: { url: `https://i.ibb.co/Kp36tPhC/1377.png` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363368882758119@newsletter',
-                    newsletterName: 'QUEEN-RASHU-MD',
+                    newsletterJid: '120363285813931317@newsletter',
+                    newsletterName: 'ZORO ZMD-UPDATES',
                     serverMessageId: 143
                 }
             }
