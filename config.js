8906df44249177f5a719fc033c3d1740453e01ec
x-kevinbro <damiruxox@outlook.com>
2025-05-02 20:34:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nnZ3CR7a#hNOYUZOh4BXJco2jxSJADhzYM7oKOM0DIw7cZ3K2WPI",
+SESSION_ID: process.env.SESSION_ID || "vmIAHThY#DEQ9L3C7Sww9lS-8BxK26v2BwdbV67iiBeckEMcKMvo",
 MONGODB: process.env.MONGODB || "mongodb://mongo:VWrehHybtAmOzcRvVFJVzPoqCdBQSiNb@maglev.proxy.rlwy.net:30273",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
