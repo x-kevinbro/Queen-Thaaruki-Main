5ee84f8f2ee1e4100ce91ea62abd1c76346b9d52
x-kevinbro <damiruxox@outlook.com>
2025-05-13 15:19:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vmIAHThY#DEQ9L3C7Sww9lS-8BxK26v2BwdbV67iiBeckEMcKMvo",
+SESSION_ID: process.env.SESSION_ID || "2vhABJwS#iuIsfI-cmJfBrtH-BKwwXUJtQ0gpvKXoYU9l8jIwFVw",
 MONGODB: process.env.MONGODB || "mongodb://mongo:VWrehHybtAmOzcRvVFJVzPoqCdBQSiNb@maglev.proxy.rlwy.net:30273",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
